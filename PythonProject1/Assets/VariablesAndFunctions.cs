e95793fe8a40f3c4ea5ed2e0277167fc1f51da30
BJ Warner <brett.j.warner@gmail.com>
2015-09-11 20:26:57 -0600

    Made Changes.

--- PythonProject1/Assets/VariablesAndFunctions.cs
+++ PythonProject1/Assets/VariablesAndFunctions.cs
@@ -1,25 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class VariablesAndFunctions : MonoBehaviour {
+public class VariablesAndFunctions : MonoBehaviour 
+{
 
-	int myInt = 75 ;
-	// Use this for initialization
-	void Start () {
+int myInt = 75 ;
+// Use this for initialization
+	void Start () 
+	{
 		myInt = MultiplyByThree (myInt);
 		Debug.Log (myInt);
 	}
 
-	int MultiplyByThree (int number){
+	int MultiplyByThree (int number)
+	{
 		int ret;
 
 		ret = number * 3;
 
 		return ret;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+
 	}
 }
