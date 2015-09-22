3f3a42785950b9ca89908212b8cbf0a2ac8feb04
BJ Warner <brett.j.warner@gmail.com>
2015-09-22 10:00:29 -0600

    Working on my If Statements. Changed coffee Temp to Explosive Pressure. Changed falling to increasing and altered the limits. Now, I need to make sure the coding is making sense with the scenario. I need to find a way to split the statements into 3 different occasions while increasing..

---
+++ PythonProject1/Assets/IfStatements.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfStatements : MonoBehaviour 
+{
+	float explosivePressure = 1.0f;
+	float upperLimitPressure = 60.0f;
+	float lowerLimitPressure = 30.0f;
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Space))
+			TemperatureTest ();
+
+		explosivePressure += Time.deltaTime * 5f;
+	}
+
+	void TemperatureTest ()
+	{
+		//if the explosive's pressure is greater than the lowest limit
+		if (explosivePressure < lowerLimitPressure) {
+			//do this
+			print ("Pressure Levels are reaching ciritcal. Evacuation is advised.");
+
+		}
+
+		// if the pressure is greater than the upper most pressure limit
+		else if (explosivePressure > upperLimitPressure) {
+			//do this
+			print ("Pressure Levels Critical: Immediate evactuation required!");
+		}
+
+		// If it's neiter of those then
+		else {
+			print ("Pressure levels are stable however, the levels are rising.");
+		}
+	}
+
+}
