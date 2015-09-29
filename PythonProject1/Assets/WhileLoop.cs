11fc7be5ce01d5b9221a0da34997fca1012b3d98
BJ Warner <brett.j.warner@gmail.com>
2015-09-29 10:00:39 -0600

    Set up the loop files and started to make some changes to them. Still trying to determine when I would use the "while" loop effectively..

---
+++ PythonProject1/Assets/WhileLoop.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhileLoop : MonoBehaviour {
+
+	int cupsInTheSink = 4;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		while (cupsInTheSink > 0)
+		{
+			print ("I've washed a cup!");
+			cupsInTheSink--;
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
