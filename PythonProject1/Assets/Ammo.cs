6333e6e38d7c87c9b49eda4cd06d5fe95097887d
BJ Warner <brett.j.warner@gmail.com>
2015-10-27 10:02:54 -0600

    Started working on the classes for the duck hunt game. Added the ammo script/class.

---
+++ PythonProject1/Assets/Ammo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ammo : MonoBehaviour {
+
+
+	public class Ammo
+	{
+
+		public int bullets;
+		public int rockets;
+		public int energy;
+
+		public Ammo ()
+		//constructor
+		{
+			bullets = 15;
+			rockets = 0;
+			energy = 0;
+
+	}
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
