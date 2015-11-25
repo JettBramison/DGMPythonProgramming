101cbdf9ce2ad954f8932d38dd18a82813f18556
BJ Warner <brett.j.warner@gmail.com>
2015-11-25 15:16:05 -0700

    Finally working on getting this game up and running.

--- Duck Hunt/Assets/Ducks.cs
+++ Duck Hunt/Assets/Ducks.cs
@@ -4,20 +4,19 @@ using System.Collections;
 public class Ducks : MonoBehaviour 
 {
 
-	var theObject = GameObject;
-	float maxPos = 2000.0;
-	float minPos = 9.0;
+	var theObject:GameObject;
+	var maxPos:float = 2000.0;
+	var minPos:float = 9.0;
 	var max = 22;
-
-
-	void start ()
+	
+	function Start()
 	{
 		StartCoroutine(spawn());
 	}
 	
 	function spawn() : IEnumerator 
 	{
-		for (int i = 0; i < max; i++) 
+		for (var i = 0; i < max; i++) 
 		{
 			yield WaitForSeconds(2.0);
 			var theNewPos= new Vector3 (Random.Range(minPos,maxPos),0,Random.Range(minPos,maxPos));
