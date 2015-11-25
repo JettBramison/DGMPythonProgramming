3d47daea8172f09dd67bc51769066013918a9bfc
BJ Warner <brett.j.warner@gmail.com>
2015-11-25 15:12:59 -0700

    I've done a lot of work...

---
+++ Duck Hunt/Assets/Ducks.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ducks : MonoBehaviour 
+{
+
+	var theObject = GameObject;
+	float maxPos = 2000.0;
+	float minPos = 9.0;
+	var max = 22;
+
+
+	void start ()
+	{
+		StartCoroutine(spawn());
+	}
+	
+	function spawn() : IEnumerator 
+	{
+		for (int i = 0; i < max; i++) 
+		{
+			yield WaitForSeconds(2.0);
+			var theNewPos= new Vector3 (Random.Range(minPos,maxPos),0,Random.Range(minPos,maxPos));
+			var go : GameObject = Instantiate(theObject);
+			go.transform.position = theNewPos;
+		}
+	}
+		
+
+}
---
+++ Duck Hunt/Assets/Reticle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Reticle : MonoBehaviour 
+{
+
+	void Update () 
+	{
+
+		Vector3 temp = Input.mousePosition;
+
+			temp.z = 10f; 
+		// Set this to be the distance you want the object to be placed in front of the camera.
+
+		this.transform.position = Camera.main.ScreenToWorldPoint(temp);
+	}
+}
