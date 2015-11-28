bdc076de270304ea5d28c8ebc90e9288b867a109
BJ Warner <brett.j.warner@gmail.com>
2015-11-27 22:39:28 -0700

    I'm working on the final project with my buddy. He's done a lot of the heavy lifting and is helping me understand what's going on.

---
+++ New Unity Project/Assets/Crosshair.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour {
+	public float crosshairScale = 1;
+	public Texture2D cursor;
+
+
+
+	// Use this for initialization
+	void Start () {
+		cursor = Resources.Load<Texture2D> ("Crosshair1");
+		Cursor.visible = false;
+		//GUI.DrawTexture(new Rect(Input.mousePosition.x, Input.mousePosition.y, cursor.width, cursor.height), cursor);
+
+	}
+
+	void OnGUI(){
+		GUI.DrawTexture(new Rect(Input.mousePosition.x - cursor.width/2 , (Screen.height - Input.mousePosition.y) - cursor.height / 2, cursor.width, cursor.height), cursor);
+
+	}
+	// Update is called once per frame
+	void Update () {
+		//if(crosshairTexture!=null)
+		//	GUI.DrawTexture(new Rect((Screen.width-crosshairTexture.width*crosshairScale)/2 ,(Screen.height-crosshairTexture.height*crosshairScale)/2, crosshairTexture.width*crosshairScale, crosshairTexture.height*crosshairScale),crosshairTexture);
+		//else
+		//	Debug.Log("No crosshair texture set in the Inspector");
+	
+	}
+}
---
+++ New Unity Project/Assets/Ducks.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ducks : MonoBehaviour 
+{
+	
+	public Texture2D theObject;
+	int max = 10;
+	private bool[] draw;
+	private Rect[] r; 
+	private bool waitingUpdate = false; 
+	private int waitTime = 5;
+	int numEscaped = 0;
+	int score = 0;
+	int maxSpeed = 2;
+	int horSpeed = 5;
+	
+	
+	void Start()
+	{
+		r = new Rect[max];
+		draw = new bool[max];
+		for(int i = 0; i < max; i++){
+			draw[i] = false;
+			//float vert = Random.Range (0, Screen.height - theObject.height);
+			//float horz = Random.Range (0, Screen.width - theObject.width);
+			//r[i] = new Rect (horz, vert, theObject.width, theObject.height);
+		}
+		
+		//theObject = Resources.Load<Texture2D> ("duck");
+		//StartCoroutine(spawn());
+		//spawn ();
+	}
+	
+	IEnumerator CheckSpawn(){	
+		yield return new WaitForSeconds(waitTime);
+		int times = Random.Range (0, max);
+		int act = 0;
+		for (int i = 0; i < max; i++) {
+			if (!draw [i]) {
+				if(act < times){
+					act++;
+					draw [i] = true;
+					float vert = Random.Range (0, Screen.height - theObject.height);
+					float horz = Random.Range (0, Screen.width - theObject.width);
+					r[i] = new Rect (horz, vert, theObject.width, theObject.height);
+				}else{
+					break;
+				}
+			}
+		}
+		waitingUpdate = false;
+	}
+	void OnGUI()
+	{
+		GUI.Label (new Rect (10, 10, 400, 50), "<size=30>Score: " + score + "</size>");
+		GUI.Label (new Rect (10, 60, 400, 50), "<size=30>Number Escaped: " + numEscaped + "</size>");
+		for (var i = 0; i < max; i++) {
+			if (draw [i]) {
+				int speed = Random.Range(0,maxSpeed);
+				int hSpeed = Random.Range(-horSpeed,horSpeed);
+				Rect tr = new Rect(r[i].xMin - hSpeed ,r[i].yMin - speed,theObject.width,theObject.height);
+				r[i] = tr;
+				if(r[i].yMin - 1 < 0){
+					draw[i] = false;
+					numEscaped+=1;
+				}else{
+					GUI.DrawTexture (r [i], theObject);
+				}
+			}
+			//yield return new WaitForSeconds(2);
+			//var theNewPos= new Vector3 (Random.Range(minPos,maxPos),0,Random.Range(minPos,maxPos));
+			//var go= Instantiate(theObject);
+			//go.transform.position = theNewPos;
+			//}
+		}
+	}
+	
+	void Update(){
+		if (!waitingUpdate) {
+			waitingUpdate = true;
+			StartCoroutine (CheckSpawn ());
+		}
+		Vector2 mousePos = new Vector2 (Input.mousePosition.x, Screen.height - Input.mousePosition.y);
+		for (int i = 0; i < max; i++) {
+			if (Input.GetMouseButtonDown (0)) {
+				if (r [i].Contains (mousePos) && draw[i]) {
+					draw [i] = false;
+					score+=1;
+				}
+			}
+		}
+	}
+}
